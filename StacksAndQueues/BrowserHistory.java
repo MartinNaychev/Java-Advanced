73149b10eca68e01cd76cca0536d3f613edd00ed
Martin Naychev <martin.naychev@gmail.com>
2025-04-09 13:25:22 +0300

    Update BrowserHistory.java

--- StacksAndQueues/BrowserHistory.java
+++ StacksAndQueues/BrowserHistory.java
@@ -1,4 +1,33 @@
 package StacksAndQueues;
 
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class BrowserHistory {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        ArrayDeque<String> urlStack = new ArrayDeque<>();
+
+        while (!input.equals("Home")) {
+
+            if (input.equals("back")) {
+                if (urlStack.size() <= 1) {
+                    System.out.println("no previous URLs");
+                } else {
+                    urlStack.pop();
+                    System.out.println(urlStack.peek());
+                }
+
+            } else {
+                urlStack.push(input);
+                System.out.println(input);
+            }
+
+            input = scanner.nextLine();
+
+        }
+
+
+    }
 }
