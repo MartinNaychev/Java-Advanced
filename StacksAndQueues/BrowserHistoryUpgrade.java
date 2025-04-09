225cb671e84241931e1d262f96cfaa639004e60e
Martin Naychev <martin.naychev@gmail.com>
2025-04-09 16:16:37 +0300

    Create BrowserHistoryUpgrade.java

---
+++ StacksAndQueues/BrowserHistoryUpgrade.java
@@ -0,0 +1,11 @@
+package StacksAndQueues;
+
+import java.util.Scanner;
+
+public class BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+    }
+}
