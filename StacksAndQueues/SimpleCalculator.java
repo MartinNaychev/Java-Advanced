2aea61efe59921e23ad8a8eb677ac6a587adf9e4
Martin Naychev <martin.naychev@gmail.com>
2025-04-09 14:29:57 +0300

    Update SimpleCalculator.java

--- StacksAndQueues/SimpleCalculator.java
+++ StacksAndQueues/SimpleCalculator.java
@@ -10,38 +10,38 @@ public class SimpleCalculator {
         ArrayDeque<Integer> numbers = new ArrayDeque<>();
         ArrayDeque<String> operators = new ArrayDeque<>();
 
-        for (int i = 0; i < input.length; i+=2) {
+        for (int i = 0; i < input.length; i += 2) {
             numbers.push(Integer.valueOf(input[i]));
         }
 
-        for (int i = 1; i < input.length; i+=2) {
+        for (int i = 1; i < input.length; i += 2) {
             operators.push(input[i]);
         }
 
         ArrayDeque<Integer> reversedNumbers = new ArrayDeque<>();
         ArrayDeque<String> reversedOperators = new ArrayDeque<>();
-        while (!numbers.isEmpty()){
+        while (!numbers.isEmpty()) {
             int number = numbers.pop();
             reversedNumbers.push(number);
         }
-        while (!operators.isEmpty()){
+        while (!operators.isEmpty()) {
             String operator = operators.pop();
             reversedOperators.push(operator);
         }
 
-       while (reversedNumbers.size()>1){
-           int firstNumber = reversedNumbers.pop();
-           String operator = reversedOperators.pop();
-           int secondNumber = reversedNumbers.pop();
-           int result = 0;
-           if (operator.equals("+")){
-               result=firstNumber + secondNumber;
-           }else {
-               result = firstNumber-secondNumber;
-           }
-           reversedNumbers.push(result);
+        while (reversedNumbers.size() > 1) {
+            int firstNumber = reversedNumbers.pop();
+            String operator = reversedOperators.pop();
+            int secondNumber = reversedNumbers.pop();
+            int result = 0;
+            if (operator.equals("+")) {
+                result = firstNumber + secondNumber;
+            } else {
+                result = firstNumber - secondNumber;
+            }
+            reversedNumbers.push(result);
 
-       }
+        }
 
         System.out.println(reversedNumbers.peek());
 
