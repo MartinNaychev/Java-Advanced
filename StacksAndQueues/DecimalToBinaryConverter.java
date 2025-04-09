b963ec21fb9cde8043a7a6dbb2c27721371ca9dc
Martin Naychev <martin.naychev@gmail.com>
2025-04-09 14:29:51 +0300

    Update DecimalToBinaryConverter.java

--- StacksAndQueues/DecimalToBinaryConverter.java
+++ StacksAndQueues/DecimalToBinaryConverter.java
@@ -8,22 +8,22 @@ public class DecimalToBinaryConverter {
         Scanner scanner = new Scanner(System.in);
 
         int number = Integer.parseInt(scanner.nextLine());
-        if (number==0){
+        if (number == 0) {
             System.out.println(0);
             return;
         }
 
         ArrayDeque<Integer> binaryNumber = new ArrayDeque<>();
 
-        while(number!=0){
-            binaryNumber.push(number%2);
-            number/=2;
+        while (number != 0) {
+            binaryNumber.push(number % 2);
+            number /= 2;
 
         }
 
         //System.out.println(binaryNumber.toString().replaceAll("[\\[\\], ]", ""));
 
-        while (!binaryNumber.isEmpty()){
+        while (!binaryNumber.isEmpty()) {
             System.out.print(binaryNumber.pop());
         }
 
