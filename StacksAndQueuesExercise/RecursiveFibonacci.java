b4c1a2853dc9920f64f82bd1f736fec11bbcfc02
Martin Naychev <martin.naychev@gmail.com>
2025-04-13 00:51:41 +0300

    Create RecursiveFibonacci.java

---
+++ StacksAndQueuesExercise/RecursiveFibonacci.java
@@ -0,0 +1,9 @@
+package StacksAndQueuesExercise;
+
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
