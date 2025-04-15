0b1d0e18f68a89fe6b2a53aad7a3d128aa3d5db0
Martin Naychev <martin.naychev@gmail.com>
2025-04-15 11:03:37 +0300

    Update BalancedParentheses.java

--- StacksAndQueuesExercise/BalancedParentheses.java
+++ StacksAndQueuesExercise/BalancedParentheses.java
@@ -8,54 +8,30 @@ public class BalancedParentheses {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         ArrayDeque<Character> openBrackets = new ArrayDeque<>();
-        ArrayDeque<Character> closeBrackets = new ArrayDeque<>();
+
 
         for (char symbol : input.toCharArray()) {
 
             if (symbol == '{' || symbol == '[' || symbol == '(') {
                 openBrackets.push(symbol);
-            } else if (symbol == '}' || symbol == ']' || symbol == ')') {
-                closeBrackets.offer(symbol);
+            } else if (openBrackets.isEmpty() || !validateCloseBracket(openBrackets.pop(), symbol)) {
+                System.out.println("NO");
+                return;
             }
+
         }
-        for (Character bracket : openBrackets) {
-            if (bracket == '{') {
-                if (closeBrackets.peek() == '}') {
-                    openBrackets.pop();
-                    closeBrackets.poll();
-                } else {
-                    System.out.println("NO");
-                    return;
-                }
-            } else if (bracket == '[') {
-                if (closeBrackets.peek() == ']') {
-                    openBrackets.pop();
-                    closeBrackets.poll();
-                } else {
-                    System.out.println("NO");
-                    return;
-                }
-
-            } else if (bracket == '(') {
-                if (closeBrackets.peek() == ')') {
-                    openBrackets.pop();
-                    closeBrackets.poll();
-                } else {
-                    System.out.println("NO");
-                    return;
-                }
-
-            }
-        }
-
-        if (openBrackets.isEmpty()) {
-            System.out.println("NO");
-            return;
-        }
-
         System.out.println("YES");
 
+    }
 
+    private static boolean validateCloseBracket(char openBracket, char closeBracket) {
+        if (openBracket == '{') {
+            return closeBracket == '}';
+        } else if (openBracket == '[') {
+            return closeBracket == ']';
+        } else {
+            return closeBracket == ')';
+        }
     }
 }
 
