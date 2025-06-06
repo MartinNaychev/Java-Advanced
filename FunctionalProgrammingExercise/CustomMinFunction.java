8d724737bf5dce575527e256099961304e449eb4
Martin Naychev <martin.naychev@gmail.com>
2025-06-06 17:13:04 +0300

    Create CustomMinFunction.java

---
+++ FunctionalProgrammingExercise/CustomMinFunction.java
@@ -0,0 +1,20 @@
+package FunctionalProgrammingExercise;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> getMinNumb = list -> Collections.min(list);
+        System.out.println(getMinNumb.apply(numbers));
+
+    }
+}
