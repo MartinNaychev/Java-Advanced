dee344204801f9ebf8608d0c003dbc4f64aef31b
Martin Naychev <martin.naychev@gmail.com>
2025-06-09 12:23:06 +0300

    Create Parent.java

---
+++ DefiningClassesExercise/Google/Parent.java
@@ -0,0 +1,15 @@
+package DefiningClassesExercise.Google;
+
+public class Parent {
+    private String name;
+    private String birthday;
+
+    public Parent(String name, String birthday) {
+        this.name = name;
+        this.birthday = birthday;
+    }
+    @Override
+    public String toString(){
+        return this.name + " " + this.birthday;
+    }
+}
