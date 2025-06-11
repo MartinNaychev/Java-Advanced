65d558b413d5b32e6edba67ede5c7bc5753d95b6
Martin Naychev <martin.naychev@gmail.com>
2025-06-11 17:15:00 +0300

    Create Main.java

---
+++ IteratorsAndComparators/Library/Main.java
@@ -0,0 +1,19 @@
+package IteratorsAndComparators.Library;
+
+import java.util.Iterator;
+
+public class Main {
+    public static void main(String[] args) {
+        Book bookOne = new Book("Animal Farm", 2003, "George Orwell");
+        Book bookThree = new Book("The Documents in the Case", 2002);
+        Book bookTwo = new Book("The Documents in the Case 2", 1930, "Dorothy Sayers", "Robert Eustace");
+        Library library = new Library(bookOne, bookTwo, bookThree);
+        //library има възможността да се обхожда
+
+        Iterator<Book> iterator = library.iterator();
+        while(iterator.hasNext()) {
+            System.out.println(iterator.next().getTitle());
+        }
+
+    }
+}
