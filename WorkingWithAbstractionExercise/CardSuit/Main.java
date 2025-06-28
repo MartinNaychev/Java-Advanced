64c8af387b922f3c4c9a8e0ed9cf88094ad7c4ea
Martin Naychev <martin.naychev@gmail.com>
2025-06-28 17:00:48 +0300

    Create Main.java

---
+++ WorkingWithAbstractionExercise/CardSuit/Main.java
@@ -0,0 +1,13 @@
+package WorkingWithAbstractionExercise.CardSuit;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Suits:");
+        Suits[] suits = Suits.values();
+
+        for (Suits suit : suits) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n",suit.ordinal(),suit.name());
+        }
+
+    }
+}
