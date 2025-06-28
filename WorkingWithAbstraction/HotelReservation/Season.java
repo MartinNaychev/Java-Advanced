a0476b4d1f526d678ee337bb9723fa5dac359ab8
Martin Naychev <martin.naychev@gmail.com>
2025-06-28 17:00:21 +0300

    Create Season.java

---
+++ WorkingWithAbstraction/HotelReservation/Season.java
@@ -0,0 +1,18 @@
+package WorkingWithAbstraction.HotelReservation;
+
+public enum Season {
+    AUTUMN(1),
+    SPRING(2),
+    WINTER(3),
+    SUMMER(4);
+
+    private int multiplayer;
+
+    Season(int multiplayer) {
+        this.multiplayer = multiplayer;
+    }
+
+    public int getMultiplayer() {
+        return this.multiplayer;
+    }
+}
