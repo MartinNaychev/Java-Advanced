650bfd6c479a875543ec177e5d4ea4ef80a955b6
Martin Naychev <martin.naychev@gmail.com>
2025-07-30 17:34:09 +0300

    Create ExperiencedPicker.java

---
+++ OOPPreparationExams/ExamOne/picking/entities/pickers/ExperiencedPicker.java
@@ -0,0 +1,9 @@
+package OOPPreparationExams.ExamOne.picking.entities.pickers;
+
+public class ExperiencedPicker extends BasePicker{
+    private static final int initialVitality = 60;
+
+    public ExperiencedPicker(String name) {
+        super(name, initialVitality);
+    }
+}
