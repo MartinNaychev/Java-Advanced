7502a91212fe06e45e985318562d946b93d9f022
Martin Naychev <martin.naychev@gmail.com>
2025-07-30 17:34:10 +0300

    Create JuniorPicker.java

---
+++ OOPPreparationExams/ExamOne/picking/entities/pickers/JuniorPicker.java
@@ -0,0 +1,11 @@
+package OOPPreparationExams.ExamOne.picking.entities.pickers;
+
+public class JuniorPicker extends BasePicker{
+
+    private static final int initialVitality = 30;
+
+
+    public JuniorPicker(String name) {
+        super(name, initialVitality);
+    }
+}
